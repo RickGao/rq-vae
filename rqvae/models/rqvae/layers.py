31a38f9f22ac3a511a23d6a115e83a7af4aba231
RickGao <rickgaoid@outlook.com>
2025-09-23 20:58:03 -0400

    Recover

--- rqvae/models/rqvae/layers.py
+++ rqvae/models/rqvae/layers.py
@@ -34,24 +34,6 @@ class Upsample(nn.Module):
             x = self.conv(x)
         return x
 
-class UpsamplePad(nn.Module):
-    def __init__(self, in_channels, with_conv):
-        super().__init__()
-        self.with_conv = with_conv
-        if self.with_conv:
-            self.conv = torch.nn.Conv2d(in_channels,
-                                        in_channels,
-                                        kernel_size=3,
-                                        stride=1,
-                                        padding=1)
-
-    def forward(self, x):
-        x = torch.nn.functional.interpolate(x, scale_factor=2.0, mode="nearest")
-        if self.with_conv:
-            x = self.conv(x)
-        x = x[:, :, :-1, :]
-        return x
-
 
 class Downsample(nn.Module):
     def __init__(self, in_channels, with_conv):
@@ -68,28 +50,6 @@ class Downsample(nn.Module):
     def forward(self, x):
         if self.with_conv:
             pad = (0,1,0,1)
-            x = torch.nn.functional.pad(x, pad, mode="constant", value=0)
-            x = self.conv(x)
-        else:
-            x = torch.nn.functional.avg_pool2d(x, kernel_size=2, stride=2)
-        return x
-
-
-class DownsamplePad(nn.Module):
-    def __init__(self, in_channels, with_conv):
-        super().__init__()
-        self.with_conv = with_conv
-        if self.with_conv:
-            # no asymmetric padding in torch conv, must do it ourselves
-            self.conv = torch.nn.Conv2d(in_channels,
-                                        in_channels,
-                                        kernel_size=3,
-                                        stride=2,
-                                        padding=0)
-
-    def forward(self, x):
-        if self.with_conv:
-            pad = (0,1,1,1)
             x = torch.nn.functional.pad(x, pad, mode="constant", value=0)
             x = self.conv(x)
         else:
